f3a71b9b5435e53246cc32f79fba280292c89bf0
leyuri <leyuri97@gmail.com>
2020-04-16 14:56:27 +0900

    Add CompanyNews

---
+++ src/components/CompanyNews.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react"
+import { API_URL, API_KEY } from './Config';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Container from '@material-ui/core/Container';
+
+
+
+
+export default function CompanyNews() {  
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetch(`${API_URL}news/IBM?token=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        setPosts(data) // new
+      })
+  }, [])
+  return (
+    <Container maxWidth=" ">
+    <List component="nav" aria-label="main mailbox folders">
+    {posts.map(item => (
+        <div key={item.title}>
+        <ListItem button onClick >
+            <ListItemText
+            primary={item.headline}
+            secondary={item.summary}
+            />
+        </ListItem>
+        <Divider/>
+        </div>
+    ))}  
+    </List>
+    </Container>
+  )
+}
