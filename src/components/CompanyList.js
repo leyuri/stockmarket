8adbd6836f59602233d4b2b9a3fbb4fb8495346c
leyuri <leyuri97@gmail.com>
2020-04-20 23:14:56 +0900

    Fetch CompanyList

--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -1,147 +1,85 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
+import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
-// import Chart from './Chart.js'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import { API_URL, API_KEY } from './Config';
 
 
 
-const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString(),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString(),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
-];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
+export default function CompanyList() {  
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+  const [companys, setCompanys] = useState([])
+  useEffect(() => {
+    fetch(`${API_URL}stock/symbol?exchange=US&token=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        setCompanys(data) // new
+      })
+  }, [])
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-  container: {
-    maxHeight: 440,
-  },
-});
-
-export default function CompanyList() {
-  
-  const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   return (
-
-
-    <React.Fragment>
-    <CssBaseline />
     <Container maxWidth=" ">
-
-      <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <List component="nav" aria-label="main mailbox folders">
+    {companys.map(item => (
+        <div key={item.symbol}>
+        <ListItem>
+            <ListItemText
+            primary={item.description}
+            secondary={item.displaySymbol}
+            />
+        </ListItem>
+        <Divider/>
+        </div>
+    ))}  
+    </List>
     </Container>
-  </React.Fragment>
-
-
-  );
+  )
 }
+
+
+// class CompanyList extends Component{
+//   componentDidMount() {
+//     this.props.fetchCompanyList();
+//   }
+
+//   render() {
+//     const arr_company = ['AAPL','IBM','ADRE'];
+//     var company_list = this.props.company_list;
+//     if (!company_list || company_list.length === 0) {
+//       return <div/>;
+//     }
+//     return (
+//       <Container maxWidth=" ">
+//       <List component="nav" aria-label="main mailbox folders">
+//       { _.map(arr_company => (
+//           <div key={arr_company}>
+//           <ListItem button onClick >
+//               <ListItemText
+//               primary={arr_company}
+//               secondary={arr_company}
+//               />
+//           </ListItem>
+//           <Divider/>
+//           </div>
+//       ))}  
+//       </List>
+//       </Container>
+//     )
+//   }
+// }
+
+// function mapStateToProps(state) {
+//   return {
+//     company_list: state.company_list.data,
+//   };
+// }
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchCompanyList }, dispatch);
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CompanyList);
